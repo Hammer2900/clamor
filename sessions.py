bc947ce6352d906d944377d6ecd97a2ce55e28f2
Falling Duck <handcrafteddoom@gmail.com>
2012-12-14 10:43:43 -0800

    Trying to get session id to bind to IP address

--- sessions.py
+++ sessions.py
@@ -13,18 +13,19 @@ class Session(object):
     def start(self):
         if not(request.get_cookie('PYSESSID')):
             sid = new('sha1', str(int(time() * 1000)) + str(randint(0, 4596))).hexdigest()
-            self.data[sid] = {}
             response.set_cookie('PYSESSID', sid)
-        if not(self.data.has_key(request.get_cookie('PYSESSID'))):
-            self.data[request.get_cookie('PYSESSID')] = {}
+            sid = new('sha1', sid + request['REMOTE_ADDR']).hexdigest()
+            self.data[sid] = {}
+        if not(request.get_cookie('PYSESSID')) or not(self.data.has_key(new('sha1', request.get_cookie('PYSESSID') + request['REMOTE_ADDR']).hexdigest())):
+            self.data[new('sha1', request.get_cookie('PYSESSID') + request['REMOTE_ADDR']).hexdigest()] = {}
     def set(self, n, v):
         try:
-            sid = request.get_cookie('PYSESSID')
+            sid = new('sha1', request.get_cookie('PYSESSID') + request['REMOTE_ADDR']).hexdigest()
             self.data[sid][n] = v
         except KeyError:
             pass
     def get(self, n):
-        sid = request.get_cookie('PYSESSID')
+        sid = new('sha1', request.get_cookie('PYSESSID') + request['REMOTE_ADDR']).hexdigest()
         try:
             return self.data[sid][n]
         except KeyError:
